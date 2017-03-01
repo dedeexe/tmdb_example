fc133c8336394814a6b1a3f00aafb5dd379e29b2
dedeexe <dede.exe@gmail.com>
2017-03-01 18:42:51 -0300

    Fix Test Script

--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -2,8 +2,15 @@
 #xcodebuild test -project ../tmdb.xcodeproj -scheme tmdb -destination 'platform=iOS Simulator,name=iPhone 6,OS=10.2' -derivedDataPath "$(pwd)" 2>&1 | tee out.txt
 
 echo "Running Tests... Wait"
-xcodebuild test -project ../tmdb.xcodeproj -scheme tmdbTests -sdk iphonesimulator -configuration Debug -enableCodeCoverage YES  -destination 'platform=iOS Simulator,name=iPhone 6,OS=10.2' > tmp.txt 
 
-echo "---> Results"
-echo "-> Success"
-cat tmp.txt | grep -E 'tmp.txt' 
+xcodebuild test -project ../tmdb.xcodeproj -scheme tmdb -sdk iphonesimulator -configuration Debug -enableCodeCoverage YES  -destination 'platform=iOS Simulator,name=iPhone 6,OS=10.2' 1>/dev/null | tee tmp.txt 
+
+echo "======= Results ======="
+echo "---> Success"
+cat tmp.txt | grep -E '^(Test Case).*(passed)' 
+echo "-----------------------"
+echo "---> Fails"
+cat tmp.txt | grep -E '^(Test Case).*(failed)' 
+echo "-----------------------"
+
+#rm tmp.txt
